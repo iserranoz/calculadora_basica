f16b0e049cccea349188c70e367cb20764897bc8
iserranoz <iserranoz35@gmail.com>
2020-06-05 01:41:59 -0500

    primeras lineas de código

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def suma(n1, n2):
+    return n1 + n2
+def resta(n1,n2):
+    return n1 - n2
+def multiplicacion(n1, n2):
+    return n1*n2
+def division(n1, n2):
+    return n1/n2
+
+
+
+print('BIENVENIDO A CALCULADORA BÁSICA')
+print('comencemos')
+opcion1 = input('introduce un valor numérico   ')
+opcion2 = input('introduce un valor numérico   ')
