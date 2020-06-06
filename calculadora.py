689a4a6232f75e5248dd971601c0dc3cf89cb00b
iserranoz <iserranoz35@gmail.com>
2020-06-05 20:41:10 -0500

    primera versión funcionando a nivel muy bássico

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- haciendo pruebas con rama
+# -*- coding: utf-8 -*-
 def suma(n1, n2):
     return n1 + n2
 def resta(n1,n2):
@@ -14,3 +14,15 @@ print('BIENVENIDO A CALCULADORA BÁSICA')
 print('comencemos')
 opcion1 = input('introduce un valor numérico   ')
 opcion2 = input('introduce un valor numérico   ')
+print('escoge la operación')
+operacion = input('1 SUMA, 2 RESTA, 3 MULTIPLICACION, 4 DIVISIÓN    ')
+if operacion == 1:
+    print(suma(opcion1, opcion2))
+elif operacion == 2:
+    print(resta(opcion1, opcion2))
+elif operacion == 3:
+    print(multiplicacion(opcion1, opcion2))
+elif operacion == 4:
+    print(division(opcion1, opcion2))
+else:
+    print('fin')
